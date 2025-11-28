2add97d2bd8bddc5c8bcc7cb35603e16a96924a1
ManojKumarMK7829 <mkmanojkumar.2297@gmail.com>
2025-11-28 16:09:59 +0530

    added datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,3 +9,8 @@ console.log(a);
 console.log(b);
 
 console.table([a, b, c, d]);
+
+/**
+ * Prefer not use var
+ * because of issue in block scope and functional scope
+ */
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,33 @@
+/**
+ * For documentation mdn
+ * ECMA script - tc39
+ */
+
+/**
+ * JavaScript has 8 data types (7 primitive + 1 non-primitive).
+ * 
+ * Primitive -> number, bigInt, String, Boolean, Undefined, null, Symbol
+ * Non Primitive -> Object
+ */
+
+/**
+ * alert(3+3) this will not work because we are using node not browser
+ */
+
+/** number */
+let age = 28
+let marks = 85.36
+
+console.log([age, marks]);
+console.log(typeof marks);
+console.log(typeof age);
+
+/**Big int - even big int is a number*/
+let bigNum = 545498465486465468484654465484684644848646446846844486489498468484988745745487487
+console.log(bigNum);
+console.log(typeof bigNum); 
+
+
+
+
+
