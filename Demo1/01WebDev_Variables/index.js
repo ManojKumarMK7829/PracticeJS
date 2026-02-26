e76a08fadda6e37ee6ce96d2d4741e2c5ddbb6e3
ManojKumarMK7829 <mkmanojkumar.2297@gmail.com>
2026-02-26 22:15:16 +0530

    Added Basic

---
+++ Demo1/01WebDev_Variables/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log("Welcome to JavaScript");
+
+// window.alert("This is an alert box!");
+document.getElementById("header").innerText = "Welcome to JavaScript";
+document.getElementById("para").innerText = "This is a paragraph.";
+document.getElementById("para").className = "para";
